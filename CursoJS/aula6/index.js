ddd28b65667a860fc5d85d96bb296a7c2082e991
Mikely Barboza <barbozamikely@gmail.com>
2024-01-26 23:25:44 -0300

    Aula 6

---
+++ CursoJS/aula6/index.js
@@ -0,0 +1,19 @@
+// Não podemos criar variáveis com palavras reservadas ex: "let if";
+// Variáveis precisam ter nomes significativos ex: let n ='campoBaseDeDados';
+// let nomeCliente = 'João';
+//Não pode começar o nome de uma variável com um número;
+// Não podem conter espaços ou traços ex: let nome-cliente ou nome cliente
+//Variavéis com nomes compostos utilizamos camelCase ex: let nomeCompleto;
+//Case-sensitive ex: letra minuscula e letra maiúscula faz diferença
+
+let nome = 'José'; //String
+let sobrenome; //Declarou a variável
+sobrenome = 'Qualquer valor'; //Inicializando a variável
+
+console.log(nome, sobrenome);
+
+console.log(nome, 'nasceu em 1989.');
+console.log('Em 2000', nome, 'conheceu Maria.');
+console.log(nome, 'casou-se com Maria em 2012.');
+console.log('Maria teve um filho com', nome, 'em 2015.');
+console.log('O filho de', nome, 'se chama Eduardo.');
